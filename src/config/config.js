8dd1b37a9dbacc687685fb48fae5559009efecef
wangwenbo <wangwenbo.89@gmail.com>
2017-11-22 09:00:52 +0800

    update readme

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 // default config
 module.exports = {
+  port: 8280,
   title: '小博叔叔FinalMe',
   subtitle: '折腾是一种态度',
   description: '我的个人网站',
-  keywords:'设计,前端',
+  keywords: '设计,前端',
   github: 'wangwenbo000',
   weibo: 'wenbocool',
   icp: '16006848号-1',
