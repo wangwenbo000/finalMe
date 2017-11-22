6d5e73217e6b77bafe9e0ffd970eab26b04531e7
wangwenbo <wangwenbo000@me.com>
2017-11-22 15:25:30 +0800

    后端config调整

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 // default config
 module.exports = {
   port: 8280,
-  title: '小博叔叔FinalMe',
+  title: '小博叔叔',
   author: '小博叔叔',
   subtitle: '折腾是一种态度',
   description: '我的个人网站',
-  keywords: '设计,前端',
+  keywords: '设计,前端,交互,实验媒体,javascript,html5,折腾',
   github: 'wangwenbo000',
   weibo: 'wenbocool',
   icp: '16006848号-1',
