e91dad71d8e2686fe05528aea1d0860c18652da6
wangwenbo <wangwenbo000@me.com>
2018-08-03 01:17:57 +0800

    页面结构调整

--- src/controller/index.js
+++ src/controller/index.js
@@ -6,6 +6,41 @@ module.exports = class extends Base {
     this.modelInstance = this.model('article');
   }
   async indexAction() {
+    var list = await this.model('category').getlist();
+    this.assign('count', list.length);
+    this.assign('cate_list', list);
+    const tags = await this.modelInstance.where({tags: ['!=', ''], show: ['NOTIN', [0, 1, 2]]}).field('tags').select();
+    let tagArr = [];
+    const tagsRes = [];
+    const dataMap = [];
+    for (const t in tags) {
+      tagArr = tagArr.concat(tags[t].tags.split('|'));
+    }
+    for (const t in tagArr) {
+      var tagJson = {};
+      if (tagsRes.indexOf(tagArr[t]) === -1) {
+        tagJson = {
+          name: tagArr[t],
+          count: 1
+        };
+        tagsRes.push(tagArr[t]);
+        dataMap.push(tagJson);
+      } else {
+        for (var ta in dataMap) {
+          if (dataMap[ta].name === tagArr[t]) {
+            dataMap[ta].count++;
+          }
+        }
+      }
+    }
+    this.assign({
+      tagsList: dataMap,
+      tagsLength: dataMap.length
+    });
+    return this.display();
+  }
+
+  async listAction() {
     const pn = this.get('pn');
     const listrows = this.config('pagesize');
     const pnTotal = await this.modelInstance
