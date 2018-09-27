9b02b53673f93c770cde8addcafa0401ad38c674
wangwenbo <wangwenbo000@me.com>
2018-09-27 21:23:40 +0800

    localstorage 缓存css js

--- www/home/gulpfile.js
+++ www/home/gulpfile.js
@@ -3,8 +3,8 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 var htmlmin = require('gulp-htmlmin');
-// var replace = require('gulp-replace');
-// var md5 = require('md5');
+var replace = require('gulp-replace');
+var md5 = require('md5');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/style.scss')
@@ -12,10 +12,11 @@ gulp.task('sass', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('home_style.css'))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('../static/css/'));
+    .pipe(gulp.dest('../static/finalme/css/'));
 });
 gulp.task('template', function() {
   return gulp.src('./*.html')
+    .pipe(replace('<{##vMd5##}>', md5(new Date().getTime())))
     .pipe(htmlmin({
       ignoreCustomFragments: [ /{%[\s\S]*?%}/, /<%[\s\S]*?%>/, /<\?[\s\S]*?\?>/ ],
       collapseBooleanAttributes: true,
@@ -47,6 +48,8 @@ gulp.task('sitemap', function() {
   return gulp.src('./*.xml')
     .pipe(gulp.dest('../../view/'));
 });
-gulp.task('sass:watch', function() {
-  gulp.watch('./sass/*.scss', ['sass', 'template', 'sitemap']);
+gulp.task('start', function() {
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./*.html', ['template']);
+  gulp.watch('./*.xml', ['sitemap']);
 });
